72eed012192bb3d066e48ad118b67b4166ff5bec
beyouwanted <2359573637@qq.com>
2023-04-08 19:52:24 +0800

    master分支修改1

--- src/main/java/com/yhy/gitdemo/controller/UserController.java
+++ src/main/java/com/yhy/gitdemo/controller/UserController.java
@@ -7,5 +7,6 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(145);
+        System.out.println(123);
     }
 }
