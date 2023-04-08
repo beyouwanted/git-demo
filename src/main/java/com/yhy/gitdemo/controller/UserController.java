e9bfe758767e9e520d0e6704eacc0ac3b968f67f
beyouwanted <2359573637@qq.com>
2023-04-08 16:00:44 +0800

    第一次提交项目到本地仓库

---
+++ src/main/java/com/yhy/gitdemo/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.yhy.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
